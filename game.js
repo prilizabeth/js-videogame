d1d9178c1ad56750f77228f10adfc18e71284095
Priscila <p.alvgonz@gmail.com>
2023-02-10 17:26:41 -0300

    modificando el canvas

--- game.js
+++ game.js
@@ -1,1 +1,13 @@
-console.log(maps);
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+window.addEventListener('load', startGame);
+
+function startGame() {
+    //game.fillRect(0,0,100,100);
+    //game.clearRect(0,0,50,20)
+    game.font = '20px'
+    game.fillStyle = 'red'
+    game.textAlign = 'left'
+    game.fillText('dami', 50, 50, 200);
+}
