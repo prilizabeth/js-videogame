dfe0a7af21312dcac2d9c6a5092fc05da4b37dde
Priscila <p.alvgonz@gmail.com>
2023-02-13 21:06:22 -0300

    canvas listo parte 2

--- game.js
+++ game.js
@@ -1,13 +1,36 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
-window.addEventListener('load', startGame);
+let canvasSize;
+let elementsSize;
+
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
+
+function setCanvasSize() {
+    if (window.innerHeight > window.innerWidth) {
+        canvasSize = window.innerWidth * 0.75;
+    } else {
+        canvasSize = window.innerHeight * 0.75;
+    }
+
+    canvas.setAttribute('width', canvasSize);
+    canvas.setAttribute('height', canvasSize);
+
+    elementsSize = (canvasSize / 10) - 1;
+
+    startGame();
+}
 
 function startGame() {
-    //game.fillRect(0,0,100,100);
-    //game.clearRect(0,0,50,20)
-    game.font = '20px'
-    game.fillStyle = 'red'
-    game.textAlign = 'left'
-    game.fillText('dami', 50, 50, 200);
+    console.log({canvasSize, elementsSize});
+
+    game.font = elementsSize + 'px Verdana';
+    game.textAlign = "";
+
+    for (let col = 0; col < 10; col++) {
+        for (let row = 1; row <= 10; row++) {
+            game.fillText(emojis['X'], elementsSize * col, elementsSize * row);
+        }
+    }
 }
